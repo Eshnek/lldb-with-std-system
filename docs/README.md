6dacccf8ea50aa12fa1da06feca13b4b25e8cabe
Eshnek <dylanpozarnsky@gmail.com>
2023-04-12 20:49:51 -0600

    +

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+An experiment figuring out how to utilize `LLDB` and `std::system` forking in a multi-threaded environment.
--- src/Main.cxx
+++ src/Main.cxx
@@ -44,7 +44,7 @@ Process 86200 stopped and restarted: thread 1 received signal: SIGCHLD
 Process 86200 exited with status = 0 (0x00000000)
 */
 
-// Failed Response, Without Mutex
+// Failed Response, Without Mutex:
 /*
 a
 Process 86473 exited with status = -1 (0xffffffff) lost connection
